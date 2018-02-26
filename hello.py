53094074dea095c4a093479687c5ad67ee00706d
Roomyster <36830738+Roomyster@users.noreply.github.com>
2018-02-26 10:10:23 -0800

    more print commands added

--- hello.py
+++ hello.py
@@ -6,4 +6,10 @@ print("Python has 3 data types:")
 print("int - Integers (12, -99)")
 print("float - Real Numbers (-90.5, 0.3)")
 print("str - Characters ('hello', '101')")
+print(" - - - ")
+homerun = 6 # homerun is now an int
+print(homerun)
+print("Python Keywords: can not be used as variable names")
+print("and.as.assert.break.class.continue.def.del.elif.else.except.false.finally.for.from.global.if.import.in.is.lambda.none.nonlocal.not.or.pass.raise.return.true.try.while.with.yield")
+
 
