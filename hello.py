6d37b73fd28e45596e00ce90ad4a7e8e4d95ec1f
Roomyster <36830738+Roomyster@users.noreply.github.com>
2018-02-26 10:31:13 -0800

    finished at variable swapping.

--- hello.py
+++ hello.py
@@ -7,9 +7,18 @@ print("int - Integers (12, -99)")
 print("float - Real Numbers (-90.5, 0.3)")
 print("str - Characters ('hello', '101')")
 print(" - - - ")
-homerun = 6 # homerun is now an int
+homerun = 6 # homerun is now an int. yay.
 print(homerun)
 print("Python Keywords: can not be used as variable names")
 print("and.as.assert.break.class.continue.def.del.elif.else.except.false.finally.for.from.global.if.import.in.is.lambda.none.nonlocal.not.or.pass.raise.return.true.try.while.with.yield")
-
-
+print("Variables can not have spaces, can't begin with $ or a digit,")
+MY_CONST = 100 # a variable whose value does not change.
+message = "item2" # going to print this variable.
+print("item1", message, "item3")
+var1, var2, var3 = "cats", "dogs", "justice"
+print("var2")
+print("Swapping values between variables. Not sure why this is cool, but okay")
+x, y = 10, 20
+print(x, y)
+x, y = 21, 11
+print(x, y)
