5624604bb04645e55ce1613957f404f42adfa094
Roomyster <36830738+Roomyster@users.noreply.github.com>
2018-02-26 09:56:55 -0800

    First tutorial (lots of prints)

---
+++ hello.py
@@ -0,0 +1,9 @@
+print("Woods are lovely dark and deep")
+print("but I have promises to keep")
+print("and miles to go before I sleep")
+print(" - - - ")
+print("Python has 3 data types:")
+print("int - Integers (12, -99)")
+print("float - Real Numbers (-90.5, 0.3)")
+print("str - Characters ('hello', '101')")
+
